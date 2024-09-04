4181e66b3dc97bf00afd38fb2d3fc12bb6db7f60
roberthsheng <roberthsheng@gmail.com>
2024-09-04 10:59:43 -0500

    added * from wrong folder

--- weframe-shared/src/lib.rs
+++ weframe-shared/src/lib.rs
@@ -1,6 +1,67 @@
 use serde::{Deserialize, Serialize};
+use std::time::Duration;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct VideoClip {
+    pub id: String,
+    pub start_time: Duration,
+    pub end_time: Duration,
+    pub track: usize,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct VideoProject {
+    pub clips: Vec<VideoClip>,
+    pub duration: Duration,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum EditOperation {
-    // Define video editing operations here
+    AddClip(VideoClip),
+    RemoveClip(String),
+    MoveClip {
+        id: String,
+        new_start_time: Duration,
+        new_track: usize,
+    },
+    TrimClip {
+        id: String,
+        new_start_time: Duration,
+        new_end_time: Duration,
+    },
+    SetProjectDuration(Duration),
+}
+
+impl VideoProject {
+    pub fn apply_operation(&mut self, op: &EditOperation) {
+        match op {
+            EditOperation::AddClip(clip) => self.clips.push(clip.clone()),
+            EditOperation::RemoveClip(id) => self.clips.retain(|c| c.id != *id),
+            EditOperation::MoveClip {
+                id,
+                new_start_time,
+                new_track,
+            } => {
+                if let Some(clip) = self.clips.iter_mut().find(|c| c.id == *id) {
+                    let duration = clip.end_time - clip.start_time;
+                    clip.start_time = *new_start_time;
+                    clip.end_time = *new_start_time + duration;
+                    clip.track = *new_track;
+                }
+            }
+            EditOperation::TrimClip {
+                id,
+                new_start_time,
+                new_end_time,
+            } => {
+                if let Some(clip) = self.clips.iter_mut().find(|c| c.id == *id) {
+                    clip.start_time = *new_start_time;
+                    clip.end_time = *new_end_time;
+                }
+            }
+            EditOperation::SetProjectDuration(new_duration) => {
+                self.duration = *new_duration;
+            }
+        }
+    }
 }
